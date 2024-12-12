6ea0ac83a7949170676330e71623f193dad906a3
bbek4210 <bhandaribibek731@gmail.com>
2024-12-13 01:03:12 +0545

    fix

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,7 +11,9 @@ const Navbar: React.FC = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
-        <img className="mx-2 w-20 h-20" src="/BibekLogo.png" alt="logo" />
+        <a href="/">
+          <img className="mx-2 w-20 h-20" src="/BibekLogo.png" alt="logo" />
+        </a>
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl cursor-pointer ">
         <a
