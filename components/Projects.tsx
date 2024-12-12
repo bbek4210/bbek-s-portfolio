37bbf16120ebaef65d39a9dda0e6c6f4c412380a
bbek4210 <bhandaribibek731@gmail.com>
2024-12-13 00:57:17 +0545

    fix

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -17,7 +17,7 @@ const Projects = () => {
       </motion.h1>
       <div>
         {PROJECTS?.map((proj, index) => (
-          <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
+          <div key={index} className="mb-8 flex flex-wrap lg:justify-center gap-6">
             <motion.div
               whileInView={{ opacity: 1, x: 0 }}
               initial={{ opacity: 0, x: -100 }}
@@ -40,14 +40,14 @@ const Projects = () => {
             className="w-full max-w-xl lg:w-3/4">
               <h6 className="mb-2 font-semibold">{proj?.title}</h6>
               <p className="mb-4 text-neutral-400">{proj?.description}</p>
-              {proj?.technologies.map((tech, index) => (
+             <div className="flex flex-wrap gap-1"> {proj?.technologies.map((tech, index) => (
                 <span
-                  className="mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-700"
+                  className="mr-2 mb-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-700 "
                   key={index}
                 >
                   {tech}
                 </span>
-              ))}
+              ))}</div>
             </motion.div>
           </div>
         ))}
